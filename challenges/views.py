ba09c8a164e42a4dddea82f3febb5860828286e6
Aloba James <alobajames1@gmail.com>
2025-12-28 23:28:54 +0100

    working with templates

---
+++ challenges/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+monthly_challenges = {
+    "january": "Eat no meat for the month",
+    "february": "Code for 300 hours",
+    "march": "Fight",
+    "april": "Eat no meat for the month 1",
+    "may": "Code for 300 hours 1",
+    "june": "Fight 1",
+    "july": "Eat no meat for the month 2",
+    "august": "Code for 300 hours 2",
+    "september": "Fight 2",
+    "october": "Eat no meat for the month 3",
+    "november": "Code for 300 hours 3",
+    "december": None,
+}
+
+# Create your views here.
+
+
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request,"challenges/challenge.html", {
+            "challengeMonth": month,
+            "text": challenge_text
+        })        
+    except:
+        return HttpResponseNotFound("<h1>Month not found!</h1>")
